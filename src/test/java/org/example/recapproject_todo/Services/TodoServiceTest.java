2120c27741eb9bce3fa91dd65f8859c1caf5ea82
ArbuzovDmtr <arbuzov.dmtr@gmail.com>
2026-03-20 16:52:46 +0100

    added createTodoTest

--- src/test/java/org/example/recapproject_todo/Services/TodoServiceTest.java
+++ src/test/java/org/example/recapproject_todo/Services/TodoServiceTest.java
@@ -1,5 +1,6 @@
 package org.example.recapproject_todo.Services;
 
+import org.example.recapproject_todo.DTO.PostTodoDTO;
 import org.example.recapproject_todo.Record.Todo;
 import org.example.recapproject_todo.Repository.TodoRepo;
 import org.example.recapproject_todo.Status.TodoStatus;
@@ -47,6 +48,25 @@ public class TodoServiceTest {
         // then
         assertEquals(expected, actual);
         verify(todoRepo, times(1)).findAll();
+        verifyNoMoreInteractions(todoRepo, idService);
+    }
+
+    @Test
+    void createTodo_shouldSaveAndReturnTodo() {
+        // given
+        PostTodoDTO dto = new PostTodoDTO("Test1", TodoStatus.IN_PROGRESS);
+        Todo expected = new Todo("abc-123", "Test1", TodoStatus.IN_PROGRESS);
+
+        when(idService.randomId()).thenReturn("abc-123");
+        when(todoRepo.save(expected)).thenReturn(expected);
+
+        // when
+        Todo actual = todoService.createTodo(dto);
+
+        // then
+        assertEquals(expected, actual);
+        verify(idService, times(1)).randomId();
+        verify(todoRepo, times(1)).save(expected);
         verifyNoMoreInteractions(todoRepo, idService);
     }
 }
